c64bdca3818564dd3f5aaf1dbe177295e258f8c6
Chukwudi <cchinwenmeri@gmail.com>
2020-06-19 15:41:34 +0100

    splash screen

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+// TODO Implement this library.
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        "Welcome to home page",
+        style: TextStyle(fontSize: 25.0),
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:team_mobileforce_gong/UI/onboarding.dart';
-
+//import 'package:team_mobileforce_gong/UI/onboarding.dart';
+import 'splashscreen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,13 +10,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'Gilroy',
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Onboarding()
+      //home: Onboarding(),
+      
     );
   }
 }
