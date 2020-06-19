fbba3070c060eb7ac32733ad56b4ed7176c586f5
Caephas <arinzeobidiegwu@gmail.com>
2020-06-19 01:24:36 +0100

    authFlow

---
+++ lib/auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider{
+  static final FirebaseAuth auth = FirebaseAuth.instance;
+  //Firebase SignUp with email and password
+  registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+  //Firebase Sign In with email and Password
+  signInWithEmail(String email, String password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+
+}
